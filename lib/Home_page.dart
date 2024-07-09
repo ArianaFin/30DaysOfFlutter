5e88dc60ba80c3b08d7ad99aa02018c0ae189c5c
brakethtml <brakethtml@gmail.com>
2024-07-09 15:26:51 +0500

    Day2 changes commit

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
+  final int Days = 30;
+  final String Name = "Areej";
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text("Catalog App"),
+        centerTitle: true,
+      ),
+      drawer: Drawer(),
+      body: Center(
+        child: Container(
+          child: Text("Hello welcome back after $Days and your name $Name"),
+        ),
+      ),
+    );
   }
 }
