bc9c13afcd438448d7670e441ef6db0ff64782b2
brakethtml <brakethtml@gmail.com>
2024-07-09 15:10:54 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+// import 'package:app/main_screen.dart';
+import 'package:app/Home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black12),
+      ),
+      home: HomePage(),
+    );
+  }
+}
