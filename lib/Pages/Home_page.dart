428c51044cf96b9e57f76322075208cb44971069
brakethtml <brakethtml@gmail.com>
2024-07-14 13:43:42 +0500

    Day8 commit

--- lib/Pages/Home_page.dart
+++ lib/Pages/Home_page.dart
@@ -11,7 +11,28 @@ class HomePage extends StatelessWidget {
         title: Text("Catalog App"),
         centerTitle: true,
       ),
-      drawer: Drawer(),
+      drawer: Drawer(
+        backgroundColor: const Color.fromARGB(255, 255, 152, 7),
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text('Areej jawaid'),
+              accountEmail: Text('areejjawaid@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('assets/images/img.jpg')),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Profile'),
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: Container(
           child: Text("Hello welcome back after $Days and your name $Name"),
